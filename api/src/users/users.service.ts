6aa900a36867cd6a9da7da6f5e589a59fc03f1e7
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2023-01-31 16:13:49 +0100

    initial commit

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,104 +1,104 @@
-import { Injectable } from '@nestjs/common';
-import { plainToClass } from 'class-transformer';
-import { CreateUserDto } from './dto/create-user.dto';
-import { GetUsersDto, UserPaginator } from './dto/get-users.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {Injectable} from '@nestjs/common';
+import {plainToClass} from 'class-transformer';
+import {CreateUserDto} from './dto/create-user.dto';
+import {GetUsersDto, UserPaginator} from './dto/get-users.dto';
+import {UpdateUserDto} from './dto/update-user.dto';
 import Fuse from 'fuse.js';
 
-import { User } from './entities/user.entity';
+import {User} from './entities/user.entity';
 import usersJson from '@db/users.json';
-import { paginate } from 'src/common/pagination/paginate';
+import {paginate} from 'src/common/pagination/paginate';
 
 const users = plainToClass(User, usersJson);
 
 const options = {
-  keys: ['name', 'type.slug', 'categories.slug', 'status'],
-  threshold: 0.3,
+    keys: ['name', 'type.slug', 'categories.slug', 'status'],
+    threshold: 0.3,
 };
 const fuse = new Fuse(users, options);
 
 @Injectable()
 export class UsersService {
-  private users: User[] = users;
+    private users: User[] = users;
 
-  create(createUserDto: CreateUserDto) {
-    return this.users[0];
-  }
-
-  async getUsers({
-    text,
-    limit,
-    page,
-    search,
-  }: GetUsersDto): Promise<UserPaginator> {
-    if (!page) page = 1;
-    if (!limit) limit = 30;
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    let data: User[] = this.users;
-
-    if (search?.replace(/%/g, '')) {
-      data = fuse.search(search)?.map(({ item }) => item);
+    create(createUserDto: CreateUserDto) {
+        return this.users[0];
     }
 
-    if (search) {
-      const parseSearchParams = search.split(';');
-      const searchText: any = [];
-      for (const searchParam of parseSearchParams) {
-        const [key, value] = searchParam.split(':');
-        // TODO: Temp Solution
-        if (key !== 'slug') {
-          searchText.push({
-            [key]: value,
-          });
+    async getUsers({
+                       text,
+                       limit,
+                       page,
+                       search,
+                   }: GetUsersDto): Promise<UserPaginator> {
+        if (!page) page = 1;
+        if (!limit) limit = 30;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        let data: User[] = this.users;
+
+        if (search?.replace(/%/g, '')) {
+            data = fuse.search(search)?.map(({item}) => item);
         }
-      }
 
-      data = fuse
-        .search({
-          $and: searchText,
-        })
-        ?.map(({ item }) => item);
+        if (search) {
+            const parseSearchParams = search.split(';');
+            const searchText: any = [];
+            for (const searchParam of parseSearchParams) {
+                const [key, value] = searchParam.split(':');
+                // TODO: Temp Solution
+                if (key !== 'slug') {
+                    searchText.push({
+                        [key]: value,
+                    });
+                }
+            }
+
+            data = fuse
+                .search({
+                    $and: searchText,
+                })
+                ?.map(({item}) => item);
+        }
+
+        const results = data.slice(startIndex, endIndex);
+        const url = `/users?limit=${limit}`;
+
+        return {
+            data: results,
+            ...paginate(data.length, page, limit, results.length, url),
+        };
     }
 
-    const results = data.slice(startIndex, endIndex);
-    const url = `/users?limit=${limit}`;
+    findOne(id: number) {
+        return this.users.find((user) => user.id === id);
+    }
 
-    return {
-      data: results,
-      ...paginate(data.length, page, limit, results.length, url),
-    };
-  }
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return this.users[0];
+    }
 
-  findOne(id: number) {
-    return this.users.find((user) => user.id === id);
-  }
+    remove(id: number) {
+        return `This action removes a #${id} user`;
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return this.users[0];
-  }
+    makeAdmin(user_id: string) {
+        return this.users.find((u) => u.id === Number(user_id));
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+    banUser(id: number) {
+        const user = this.users.find((u) => u.id === Number(id));
 
-  makeAdmin(user_id: string) {
-    return this.users.find((u) => u.id === Number(user_id));
-  }
+        user.is_active = !user.is_active;
 
-  banUser(id: number) {
-    const user = this.users.find((u) => u.id === Number(id));
+        return user;
+    }
 
-    user.is_active = !user.is_active;
+    activeUser(id: number) {
+        const user = this.users.find((u) => u.id === Number(id));
 
-    return user;
-  }
+        user.is_active = !user.is_active;
 
-  activeUser(id: number) {
-    const user = this.users.find((u) => u.id === Number(id));
-
-    user.is_active = !user.is_active;
-
-    return user;
-  }
+        return user;
+    }
 }
